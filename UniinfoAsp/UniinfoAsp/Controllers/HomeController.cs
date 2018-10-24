765bc5c2b0bda4b481dc38e8ba5c50f48cbe9609
Jorge W. Santos <jorgeewillian00@hotmail.com>
2018-10-24 11:08:57 -0300

    Reformulando o design do HomeController

--- UniinfoAsp/UniinfoAsp/Controllers/HomeController.cs
+++ UniinfoAsp/UniinfoAsp/Controllers/HomeController.cs
@@ -12,19 +12,6 @@ namespace UniinfoAsp.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
