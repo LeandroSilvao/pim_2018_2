4f4dce19cba9958c43474ce8a00475d086937ecd
Jorge W. Santos <jorgeewillian00@hotmail.com>
2018-10-24 11:50:37 -0300

    Criando AccountController, página de login

---
+++ UniinfoAsp/UniinfoAsp/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using UniinfoAsp.Models;
+
+namespace UniinfoAsp.Controllers
+{
+    public class AccountController : Controller
+    {
+        UnipEntities db = new UnipEntities();
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Loginn log)
+        {
+            return View();
+        }
+    }
+}
--- UniinfoAsp/UniinfoAsp/Controllers/FuncController.cs
+++ UniinfoAsp/UniinfoAsp/Controllers/FuncController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using UniinfoAsp.Models;
 
 namespace UniinfoAsp.Controllers
 {
