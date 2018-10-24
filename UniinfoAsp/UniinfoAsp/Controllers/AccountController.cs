191312c933b673df93cca7207321fe03a44aeea1
Jorge W. Santos <jorgeewillian00@hotmail.com>
2018-10-24 15:50:31 -0300

    Página de Login finalizada

--- UniinfoAsp/UniinfoAsp/Controllers/AccountController.cs
+++ UniinfoAsp/UniinfoAsp/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
 using UniinfoAsp.Models;
 
 namespace UniinfoAsp.Controllers
@@ -9,12 +11,41 @@ namespace UniinfoAsp.Controllers
         
         public ActionResult Login()
         {
+            ViewBag.Message = "";
             return View();
         }
+
         [HttpPost]
         public ActionResult Login(Loginn log)
         {
-            return View();
+            var result = db.Loginns.Where(a => a.login == log.login && a.senha == log.senha).ToList();
+            if (result.Count() > 0)
+            {
+                Session["idLogin"] = result[0].idLogin;
+                FormsAuthentication.SetAuthCookie(result[0].login, false);
+                //If admin
+                if (result[0].idNivelAcesso == 1)
+                {
+                    return RedirectToAction("../Admin/index");
+                }
+                //If Funcionario
+                if (result[0].idNivelAcesso == 2)
+                {
+                    return RedirectToAction("../Func/index");
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Erro, Usuario ou senha incorreto";
+            }
+            return View(log);
+        }
+
+        public ActionResult LogOut()
+        {
+            Session["idLogin"] = 0;
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
         }
     }
 }
